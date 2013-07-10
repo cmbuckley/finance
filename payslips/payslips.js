c2ed4fc146d622c81701f5aab088d7bcfe9c3353
Chris Buckley <chris@cmbuckley.co.uk>
2013-07-10 22:20:56 +0100

    Expose formatters, multiple accounts, test file

--- payslips/payslips.js
+++ payslips/payslips.js
@@ -1,16 +1,79 @@
 (function(doc){
-    var type = 'qif'; // type
-    var file = []; // file
+    var formatters = {
+        csv: function (rows) {
+            return rows.map(function(row) {
+                return [
+                    row.d.replace(/\d\d(\d\d)$/, '$1'),
+                    row.p,
+                    ,
+                    ,
+                    row.m,
+                    (row.a / 100).toFixed(2),
+                    row.c
+                ].join(';');
+            }).join('\n');
+        },
+        qif: function (rows) {
+            var transfers = {
+                'SAYE 2012 3YR': 'ShareSave'
+            };
+
+            function _account(records, name, type) {
+                type = type || 'Bank';
+                var head = [
+                    '!Account',
+                    'N' + name,
+                    'T' + type,
+                    '^',
+                    '!Type:' + type
+                ].join('\n');
+
+                return [head].concat(records.map(function (record) {
+                    var category = record.l || (transfers[record.m] ? '[' + transfers[record.m] + ']' : record.c);
+                    return ['D' + record.d, 'T' + (record.a / 100).toFixed(2), 'M' + record.m, 'L' + category, '^'].join('\n');
+                })).join('\n');
+            }
+
+            function _transfers(records, from, to, type) {
+                return _account(records.map(function (record) {
+                    if (transfers[record.m] !== to) {
+                        return false;
+                    }
+
+                    record.l = '[' + from + ']';
+                    record.a *= -1;
+                    return record;
+                }).filter(function (record) {
+                    return (record !== false);
+                }), to, type);
+            }
+
+            return [
+                _account(rows, 'Payslips'),
+                _transfers(rows, 'Payslips', 'ShareSave')
+            ].join('\n');
+        }
+    };
+
     var id = doc.getElementById.bind(doc);
     var sel = doc.querySelectorAll.bind(doc);
-    var date = id('lblHeading1').innerText.replace(
+    var date = id('lblHeading1');
+
+    if (!date) {
+        return window.formatters = formatters;
+    }
+
+    date = date.innerText.replace(
         /.+the (\d\d) (\w{3}).* (\d{4}).*$/,
         function(_, d, m, y) {
             return d + '-' + ("0" + ['_', 'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'].indexOf(m)).substr(-2) + '-' + y;
         }
     );
 
+    var file = [];
+    var type = 'qif';
     var payments = sel('[id^=lblPD]:not(:empty), [id^=lblDD]:not(:empty)');
+
     for (var payment in payments) {
         if (payments.propertyIsEnumerable(payment)) {
             var amount = getAmount(payments[payment]);
@@ -28,7 +91,8 @@
     }
 
     function getAmount(element) {
-        return {'P':'', 'D':'-'}[element.id[3]] + id(element.id.replace(/(lbl.)D(\d+)/, '$1A$2')).innerText;
+        var label = id(element.id.replace(/(lbl.)D(\d+)/, '$1A$2'));
+        return {'P': 100, 'D': -100}[element.id[3]] * label.innerText.replace(',', '');
     }
 
     function getPaymode(text) {
@@ -46,46 +110,6 @@
         }[text] || '';
     }
 
-    var formatters = {
-        csv: function (rows) {
-            return rows.map(function(row) {
-                return [row.d.replace(/\d\d(\d\d)$/, '$1'), row.p, , , row.m, row.a, row.c].join(';');
-            }).join('\n');
-        },
-        qif: function (rows) {
-            var transfers = {
-                'SAYE 2012 3YR': 'ShareSave'
-            };
-
-            function _account(records, name, type) {
-                type = type || 'Bank';
-                var head = ['!Account', 'N' + name, 'T' + type, '^', '!Type:' + type].join('\n');
-
-                return [head].concat(records.map(function (record) {
-                    var category = (transfers[record.m] ? '[' + transfers[record.m] + ']' : record.c);
-                    return ['D' + record.d, 'T' + record.a, 'M' + record.m, 'L' + category, '^'].join('\n');
-                })).join('\n');
-            }
-
-            function _transfers(records, from, to, type) {
-                return _account(records.map(function (record) {
-                    if (transfers[record.m] !== to) {
-                        return false;
-                    }
-
-                    record.c = '[' + from + ']';
-                    return record;
-                }).filter(function (record) {
-                    return (record !== false);
-                }), to, type);
-            }
-
-            return [
-                _account(rows, 'Payslips'),
-                _transfers(rows, 'Payslips', 'ShareSave')
-            ].join('\n');
-        }
-    }
 
     var a = doc.createElement('a');
     a.download = date + '.' + type;
