7cb2e665c91191bbfc8025bff772bf3d2984e580
Chris Buckley <chris@cmbuckley.co.uk>
2013-07-10 13:14:41 -0700

    QIF transfers

--- payslips/payslips.js
+++ payslips/payslips.js
@@ -47,16 +47,43 @@
     }
 
     var formatters = {
-        csv: function (f) {
-            return f.map(function(r) {
-                return [r.d.replace(/\d\d(\d\d)$/, '$1'), r.p, , , r.m, r.a, r.c].join(';');
+        csv: function (rows) {
+            return rows.map(function(row) {
+                return [row.d.replace(/\d\d(\d\d)$/, '$1'), row.p, , , row.m, row.a, row.c].join(';');
             }).join('\n');
         },
-        qif: function (f) {
-            var h = ['!Account', 'NPayslips', 'TAsset', '^', '!Type:Oth A'].join('\n');
-            return [h].concat(f.map(function (r) {
-                return ['D' + r.d, 'T' + r.a, 'M' + r.m, 'L' + r.c, '^'].join('\n');
-            })).join('\n');
+        qif: function (rows) {
+            var transfers = {
+                'SAYE 2012 3YR': 'ShareSave'
+            };
+
+            function _account(records, name, type) {
+                type = type || 'Bank';
+                var head = ['!Account', 'N' + name, 'T' + type, '^', '!Type:' + type].join('\n');
+
+                return [head].concat(records.map(function (record) {
+                    var category = (transfers[record.m] ? '[' + transfers[record.m] + ']' : record.c);
+                    return ['D' + record.d, 'T' + record.a, 'M' + record.m, 'L' + category, '^'].join('\n');
+                })).join('\n');
+            }
+
+            function _transfers(records, from, to, type) {
+                return _account(records.map(function (record) {
+                    if (transfers[record.m] !== to) {
+                        return false;
+                    }
+
+                    record.c = '[' + from + ']';
+                    return record;
+                }).filter(function (record) {
+                    return (record !== false);
+                }), to, type);
+            }
+
+            return [
+                _account(rows, 'Payslips'),
+                _transfers(rows, 'Payslips', 'ShareSave')
+            ].join('\n');
         }
     }
 
