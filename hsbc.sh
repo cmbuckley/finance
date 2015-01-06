25eb6df7cc9b1d79cc1432c669631ddca0386f41
Chris Buckley <chris@cmbuckley.co.uk>
2015-01-06 23:40:42 +0000

    Support globbing in hsbc script

--- hsbc.sh
+++ hsbc.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-{ rm "$1" && awk -f "$(dirname "$0")/hsbc.awk" > "$1"; } < "$1"
+script="$(dirname "$0")/hsbc.awk"
+
+for file in "$@"; do
+    if [ -e "$file" ]; then
+        { rm "$file" && awk -f "$script" > "$file"; } < "$file"
+    else
+        echo "$file: No such file or directory" >&2
+        exit 1
+    fi
+done
