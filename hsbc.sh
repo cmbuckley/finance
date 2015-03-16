3d900c59da219d96b35f21fac2d0ebee1955be0d
Chris Buckley <chris@cmbuckley.co.uk>
2015-03-16 21:16:54 +0000

    Add -i (in-place) flag to HSBC script

--- hsbc.sh
+++ hsbc.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
 script="$(dirname "$0")/hsbc.awk"
+inplace=0
+
+if [ "$1" = "-i" ]; then
+    inplace=1
+    shift
+fi
 
 for file in "$@"; do
     if [ -e "$file" ]; then
-        { rm "$file" && awk -f "$script" > "$file"; } < "$file"
+        if [ $inplace -eq 1 ]; then
+            { rm "$file" && awk -f "$script" > "$file"; } < "$file"
+        else
+            awk -f "$script" "$file"
+        fi
     else
         echo "$file: No such file or directory" >&2
         exit 1
