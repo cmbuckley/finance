05713611ff3febadd2069080649b2cfab37c0041
Chris Buckley <chris@cmbuckley.co.uk>
2015-03-30 23:11:29 +0100

    Betting bookmarklet (fix URL for payslips too)

---
+++ betting/bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function(d){d.body.appendChild(d.createElement('script')).src='https://rawgit.com/cmbuckley/finance/master/betting/app.js';})(document);
--- payslips/bookmarklet.js
+++ payslips/bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function(d){d.body.appendChild(d.createElement('script')).src='https://raw.github.com/starsquare/finance/master/payslips/payslips.js';})(document);
+javascript:(function(d){d.body.appendChild(d.createElement('script')).src='https://raw.github.com/cmbuckley/finance/master/payslips/payslips.js';})(document);
