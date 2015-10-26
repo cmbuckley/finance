f50156e91a2c5dc41c21a9fa9381313a74a0e923
Chris Buckley <chris@cmbuckley.co.uk>
2015-10-26 23:25:05 +0000

    Loop through all relevant accounts

--- src/download.js
+++ src/download.js
@@ -4,6 +4,7 @@ var Casper  = require('casper'),
     cu      = require('clientutils').create(utils.mergeObjects({}, casper.options)),
     fs      = require('fs'),
     options = utils.mergeObjects(JSON.parse(fs.read('config/download.json')), casper.cli.options),
+    output  = '',
     accounts;
 
 accounts = {
@@ -47,6 +48,10 @@ function save(filename, contents) {
     }).filter(Boolean).join('\n'));
 }
 
+function newAccount(accountName, transactions) {
+    return '!Account\nN' + accountName + '\nTBank\n^\n' + transactions;
+}
+
 function getLink(text, selector) {
     return Casper.selectXPath('//' + (selector || 'a') + '[contains(text(), "' + text + '")]');
 }
@@ -60,6 +65,7 @@ casper.start('http://www.hsbc.co.uk/1/2/personal/pib-home', function () {
     this.fill('#logonForm', {userid: options.credentials.userid}, true);
 });
 
+// occasional interstitial page
 casper.then(function () {
     if (this.exists('#tempForm')) {
         this.echo('Submitting login form');
@@ -72,6 +78,7 @@ casper.then(function () {
     this.clickLabel('Without Secure Key');
 });
 
+// password form
 casper.then(function () {
     var values = {
         memorableAnswer: options.credentials.memorableAnswer,
@@ -88,41 +95,68 @@ casper.then(function () {
 });
 
 casper.then(function () {
-    this.echo('Opening Current Account');
-    var form = this.getElementAttribute('form[action$="transaction"]', 'id');
-    this.fill('#' + form, {}, true);
+    // loop over the accounts (we'll just use the index here)
+    this.getElementsInfo('form[action$="recent-transaction"]').forEach(function (account, accountIndex) {
+        // needed if we've previously been on a download page
+        this.then(function () {
+            this.clickLabel('My accounts');
+        });
+
+        this.then(function () {
+            // re-evaluate element info for this page - form IDs change each time
+            account = this.getElementsInfo('form[action$="recent-transaction"]')[accountIndex];
+            this.echo('Opening "' + account.html.match(/<a[^>]*>([^<]*)<\/a>/)[1] + '" account (item ' + accountIndex + ')');
+            this.fill('#' + account.attributes.id, {}, true);
+        });
+
+        this.then(function () {
+            var from = new Date(options.from),
+                to   = (options.to ? new Date(options.to) : new Date());
+
+            this.echo('  Selecting dates: ' + options.from + ' - ' + (options.to || 'today'));
+
+            // all javascript form submission does here is validate the form
+            this.fill('.containerMain form', {
+                fromDateDay:   from.getDate(),
+                fromDateMonth: from.getMonth() + 1,
+                fromDateYear:  from.getFullYear(),
+                toDateDay:     to.getDate(),
+                toDateMonth:   to.getMonth() + 1,
+                toDateYear:    to.getFullYear()
+            }, true);
+        });
+
+        this.then(function () {
+            // download transactions (need xpath because of trailing whitespace)
+            var download = getLink('Download transactions')
+
+            // check for link (missing if no transactions)
+            if (this.exists(download)) {
+                this.click(download);
+
+                this.then(function () {
+                    this.echo('  Selecting QIF format');
+                    this.fill('.containerMain form', {
+                        downloadType: 'M_QIF'
+                    }, true);
+                });
+
+                this.then(function () {
+                    this.echo('  Downloading file');
+                    output += newAccount(this.getHTML('.hsbcAccountType'), getContents(this.getElementAttribute('.containerMain form', 'action'), 'POST'));
+                });
+            }
+            else {
+                this.echo('  No transactions in date range');
+            }
+        });
+    }, this);
 });
 
 casper.then(function () {
-    var from = new Date(options.from),
-        to   = (options.to ? new Date(options.to) : new Date());
-
-    this.echo('Selecting dates: ' + options.from + ' - ' + (options.to || 'today'));
-
-    // all javascript form submission does here is validate the form
-    this.fill('.containerMain form', {
-        fromDateDay:   from.getDate(),
-        fromDateMonth: from.getMonth() + 1,
-        fromDateYear:  from.getFullYear(),
-        toDateDay:     to.getDate(),
-        toDateMonth:   to.getMonth() + 1,
-        toDateYear:    to.getFullYear()
-    }, true);
-});
-
-// download transactions (need xpath because of trailing whitespace)
-casper.thenClick(getLink('Download transactions'));
-
-casper.then(function () {
-    this.echo('Selecting QIF format');
-    this.fill('.containerMain form', {
-        downloadType: 'M_QIF'
-    }, true);
-});
-
-casper.then(function () {
-    this.echo('Downloading file');
-    save(options.filename, getContents(this.getElementAttribute('.containerMain form', 'action'), 'POST'));
+    this.echo('Logging out');
+    this.clickLabel('Log off');
+    save(options.filename, output);
 });
 
 casper.run();
