304bb356a2dee5a5c34b6d1a2a112209ce8e95ef
Chris Buckley <chris@cmbuckley.co.uk>
2016-11-23 21:44:32 +0000

    Output Monzo QIF

--- src/monzo.js
+++ src/monzo.js
@@ -3,6 +3,39 @@ var fs = require('fs'),
     args = require('yargs').argv,
     config = JSON.parse(fs.readFileSync('config/monzo.json'));
 
+var categories = {
+    general:       '', // TODO inspect
+    eating_out:    'Food:Eating Out',
+    expenses:      'Job Expenses', // TODO expand
+    bills:         'Bills',
+    entertainment: 'Nights Out',
+    groceries:     'Food:Groceries',
+    shopping:      '', // TODO inspect
+    holidays:      '', // TODO inspet
+
+    cash: lookup('local_currency', {
+        '[Cash]':  'GBP',
+        '[Euros]': 'EUR'
+    }),
+    transport: lookup('description', {
+        'Car:Parking': 'NCP LIMITED'
+    }),
+    mondo: function (transaction) {
+        return (transaction.metadata.is_topup ? '[Current Account]' : '');
+    }
+};
+
+function lookup(key, matches, defaultValue) {
+    return function (transaction) {
+        return Object.keys(matches).find(function (match) {
+            var pattern = matches[match],
+                value = transaction[key];
+
+            return (pattern instanceof RegExp ? pattern.test(value) : value.includes(pattern));
+        }) || defaultValue || '';
+    };
+}
+
 function exit(scope) {
     return function (err) {
         console.error('Error with', scope, err);
@@ -14,12 +47,60 @@ function timestamp(date) {
     return date ? date + 'T00:00:00Z' : undefined;
 }
 
+function date(timestamp) {
+    return timestamp.substr(0, 10);
+}
+
+function category(transaction) {
+    var category = (categories.hasOwnProperty(transaction.category)
+                 ? categories[transaction.category]
+                 : transaction.category);
+
+    return ((typeof category == 'function') ? category(transaction) : category);
+}
+
+function payee(transaction) {
+    if (transaction.counterparty.name) {
+        return transaction.counterparty.name;
+    }
+
+    return '';
+}
+
 monzo.accounts(config.token).then(function (response) {
     monzo.transactions({
       account_id: response.accounts[0].id,
       since:      timestamp(args.from),
       before:     timestamp(args.to)
     }, config.token).then(function (response) {
-        console.log(reponse.transactions);
+        var head = [
+            '!Account',
+            'NMonzo',
+            'TBank',
+            '^',
+            '!Type:Bank'
+        ];
+
+        var qif = response.transactions.reduce(function (file, transaction) {
+            if (
+                transaction.decline_reason // failed
+                || (args.topup === false && transaction.metadata.is_topup) // ignore topups
+            ) {
+                return file;
+            }
+
+            return file.concat([
+                'D' + date(transaction.created),
+                'T' + (transaction.amount / 100).toFixed(2),
+                'M' + (transaction.notes || transaction.description.replace(/ +/g, ' ')),
+                'P' + payee(transaction),
+                'L' + category(transaction),
+                'N' + transaction.dedupe_id,
+                '^'
+            ]);
+        }, head).join('\n');
+
+        fs.writeFileSync('monzo.qif', qif);
+        console.log('Wrote transactions to monzo.qif');
     }).catch(exit('transactions'));
 }).catch(exit('accounts'));
