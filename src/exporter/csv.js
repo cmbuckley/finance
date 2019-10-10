3bcbf5cde511b33b469dd98da3ccd6044da1ab2a
Chris Buckley <chris@cmbuckley.co.uk>
2019-10-10 09:22:29 +0100

    CSV exporter supports transfers

--- src/exporter/csv.js
+++ src/exporter/csv.js
@@ -2,18 +2,20 @@ var stringify = require('csv-stringify');
 
 module.exports = function csv(transactions, options, callback) {
     var head = [
+        'Account',
         'Date',
         'Payee',
         'Amount',
         'Category',
         'Currency',
         'Rate',
-        'Comments',
+        'Notes',
         'Number',
     ];
 
-    stringify([head].concat(transactions.map(function (transaction) {
-        return [
+    stringify(transactions.reduce(function (rows, transaction) {
+        let row = [
+            options.account,
             transaction.date,
             transaction.payee,
             (transaction.localAmount / 100).toFixed(2),
@@ -23,7 +25,20 @@ module.exports = function csv(transactions, options, callback) {
             transaction.memo,
             transaction.id
         ];
-    })), {
+
+        // duplicate the row for the transfer
+        if (transaction.transfer) {
+            let transfer = row.slice(0);
+            transfer[head.indexOf('Account')] = transaction.transfer;
+            transfer[head.indexOf('Amount')] = (-transaction.localAmount / 100).toFixed(2);
+            transfer[head.indexOf('Category')] = 'Transfer ' + (transaction.localAmount < 0 ? 'from' : 'to') + ':' + options.account;
+            row[head.indexOf('Category')] = 'Transfer ' + (transaction.localAmount > 0 ? 'from' : 'to') + ':' + transaction.transfer;
+            rows.push(transfer);
+        }
+
+        rows.push(row);
+        return rows;
+    }, [head]), {
         delimiter: options.delimiter
     }, callback);
 };
