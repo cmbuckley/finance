10cbf1644d07d2c2bf9ec1acb7fdb6d81358695b
Chris Buckley <chris@cmbuckley.co.uk>
2024-02-13 18:01:58 +0000

    PayPal transaction pagination
    PayPal API requests cannot span more than 31 days, so needs to loop
    over the time window on 30-day increments.

--- src/adapter/paypal.js
+++ src/adapter/paypal.js
@@ -13,37 +13,55 @@ class PayPalAdapter extends Adapter {
                 }
             });
 
-        let response;
+        let transactions = [],
+            start = from,
+            end,
+            response;
 
-        try {
-            response = await client.get('/v1/reporting/transactions', {
-                params: {
-                    start_date: from.toISOString(),
-                    end_date: to.toISOString(),
-                    fields: ['transaction_info', 'cart_info', 'payer_info'].join(','),
-                    page_size: 500,
-                },
+        do {
+            end = moment.min(to, start.clone().add(30, 'days')),
+
+            this.logger.verbose('Searching date range', {
+                from: start.format('YYYY-MM-DD'),
+                to: end.format('YYYY-MM-DD'),
             });
-        } catch (err) {
-            this.logger.debug('Request params', err.config.params);
-            this.logger.debug(err.response.data);
 
-            let message = err.response.data.message;
-            if (err.response.data.details.length == 1) {
-                message = err.response.data.details[0].issue;
+            try {
+                response = await client.get('/v1/reporting/transactions', {
+                    params: {
+                        start_date: start.toISOString(),
+                        end_date: end.toISOString(),
+                        fields: ['transaction_info', 'cart_info', 'payer_info'].join(','),
+                        page_size: 500,
+                    },
+                });
+            } catch (err) {
+                this.logger.debug('Request params', err.config.params);
+                this.logger.debug(err.response.data);
+
+                let message = err.response.data.message;
+                if (err.response.data.details.length == 1) {
+                    message = err.response.data.details[0].issue;
+                }
+
+                throw new Error(message);
             }
 
-            throw new Error(message);
-        }
+            if (!response.data.transaction_details) {
+                throw new Error('No transactions found', {cause: response.data});
+            }
 
-        if (!response.data.transaction_details) {
-            throw new Error('No transactions found', {cause: response.data});
-        }
+            start = end.clone().add(1, 'days');
 
-        return response.data.transaction_details.map(transaction => {
-            this.logger.silly('Raw transaction', transaction);
-            return new Transaction(this.config.name, transaction, this, this.logger);
-        });
+            transactions = transactions.concat(response.data.transaction_details.map(transaction => {
+                this.logger.silly('Raw transaction', transaction);
+                return new Transaction(this.config.name, transaction, this, this.logger);
+            }));
+        } while (end.isBefore(to, 'day'));
+
+        this.logger.verbose(`Retrieved ${transactions.length} transactions`);
+        return transactions;
+
     }
 
     getDefaultConfig() {
